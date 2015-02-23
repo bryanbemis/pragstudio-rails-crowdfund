5c2c6fc82bc2707269f188353646c079f2557290
Bryan Bemis <bryan@bryanbemis.com>
2015-02-23 11:25:12 -0800

    Format pledging end date from now in words, or as done

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,11 @@
 module ProjectsHelper
+  def format_pledging_ends_on(movie)
+    if movie.pledging_ends_on > Time.now
+      # Hasn't ended yet
+      distance_of_time_in_words(Time.now, movie.pledging_ends_on).capitalize + " remaining"
+    else
+      # Ended
+      content_tag(:strong, "All Done!")
+    end
+  end
 end
