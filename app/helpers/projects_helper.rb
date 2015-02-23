e86f5de0ff6b01edfbbece8fa763fcfb6d0de81a
Bryan Bemis <bryan@bryanbemis.com>
2015-02-23 11:38:23 -0800

    Refactor logic to determine if project expired (from ProjectsHelper to Project model)

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,11 +1,11 @@
 module ProjectsHelper
-  def format_pledging_ends_on(movie)
-    if movie.pledging_ends_on > Time.now
-      # Hasn't ended yet
-      distance_of_time_in_words(Time.now, movie.pledging_ends_on).capitalize + " remaining"
-    else
+  def format_pledging_ends_on(project)
+    if project.expired?
       # Ended
       content_tag(:strong, "All Done!")
+    else
+      # Hasn't ended yet
+      distance_of_time_in_words(Time.now, project.pledging_ends_on).capitalize + " remaining"
     end
   end
 end
